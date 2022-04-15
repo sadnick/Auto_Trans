bc79ba56ff243534128c51e7149553ff0f600e99
sadnick <sadyrinnk@whatever.ru>
2022-04-15 10:50:20 +0300

    Добавьте файлы проекта.

---
+++ Auto_Trans/AddEditPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auto_Trans
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Ticket _currentTicket = new Ticket();  
+
+        public AddEditPage(Ticket selectedTicket)
+        {
+            
+            InitializeComponent();
+            if (selectedTicket != null)
+                _currentTicket = selectedTicket;
+            DataContext = _currentTicket;        
+            ComboSity.ItemsSource = AutoBaseEntities.GetContext().Ticketroutes.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentTicket.id == 0)
+                AutoBaseEntities.GetContext().Tickets.Add(_currentTicket);
+            
+            try
+            {
+                AutoBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Auto_Trans/TicketPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auto_Trans
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class TicketPage : Page
+    {
+        public TicketPage()
+        {
+            InitializeComponent();
+            ///DGridTicket.ItemsSource = AutoBaseEntities.GetContext().Tickets.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Ticket));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var TicketForRemoving = DGridTicket.SelectedItems.Cast<Ticket>().ToList();
+            if(MessageBox.Show($"Вы точно хотите удалить слудущее {TicketForRemoving.Count()} элементов?","внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    AutoBaseEntities.GetContext().Tickets.RemoveRange(TicketForRemoving);
+                    AutoBaseEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                    DGridTicket.ItemsSource = AutoBaseEntities.GetContext().Tickets.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                AutoBaseEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridTicket.ItemsSource = AutoBaseEntities.GetContext().Tickets.ToList();
+
+
+            }
+        }
+    }
+}
