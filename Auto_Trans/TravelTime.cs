a12260af1e37cb9e9824f1034e58ab7296925e42
sadnick <sadyrinnk@whatever.ru>
2022-05-25 19:56:57 +0300

    unittest

---
+++ Auto_Trans/TravelTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Trans
+{
+    public class TravelTime
+    {
+        public TimeSpan GetTravelTime(DateTime d1, DateTime d2)
+        {
+            return d2 - d1;
+        }
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Auto_Trans;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var TravelTime = new TravelTime();
+            var d1 = DateTime.Now;
+            var d2 = DateTime.Now.AddDays(10);
+            var timeTravel = d2 - d1;
+
+            var result = TravelTime.GetTravelTime(d1, d2);
+
+            Assert.AreEqual(timeTravel, result);
+        }
+    }
+}
+
